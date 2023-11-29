6b73c772a8af32b9bec3d58546e592dc4ecfda4b
ParthaMalakar <parthamalakar4321@gmail.com>
2023-11-29 12:36:52 +0600

    homepage usages part created

---
+++ src/pages/Home/Usage/Usage.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
+const Usage = () => {
+    const [count, setCount] = useState(0)
+    const [Man, setMen] = useState(0)
+    const [countdelavery, setCountdelavery] = useState(0)
+    useEffect(() => {
+        fetch('http://localhost:5000/usercount')
+            .then(res => res.json())
+            .then(data => setCount(data.count))
+    }, [])
+    useEffect(() => {
+        fetch('http://localhost:5000/parcelCount')
+            .then(res => res.json())
+            .then(data => setCountdelavery(data.count))
+    }, [])
+    useEffect(() => {
+        fetch('http://localhost:5000/all/men')
+            .then(res => res.json())
+            .then(data => setMen(data.man))
+    }, [])
+    return (
+        <div className='grid grid-cols-3 max-w-5xl mx-auto gap-3'>
+            <div className='p-8 bg-red-300 font-bold text-xl'>
+              <h1>Number of Parcel Booked :{countdelavery}</h1>
+            </div>
+            <div className='p-8 bg-red-300 font-bold text-xl'>
+                <h1>Number of Parcel Delivered:{Man}</h1>
+            </div>
+            <div className='p-8 bg-red-300 font-bold text-xl'>
+                <h2>Number of People Using Your App: {count}</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Usage;
